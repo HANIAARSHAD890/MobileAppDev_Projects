ff4683bf351a8d2ad8f90c7a98fef97a6f23e806
Hania Arshad <haniaarshad890@gmail.com>
2026-02-25 10:17:01 +0500

    Register&loginScreen

--- EmptyViews_Screen/app/src/main/java/com/example/emptyviews_screen/LoginScreen.kt
+++ EmptyViews_Screen/app/src/main/java/com/example/emptyviews_screen/LoginScreen.kt
@@ -1,20 +1,52 @@
 package com.example.emptyviews_screen
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginScreen : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    companion object {
+        const val SHARED_PREFS = "shared_prefs"
+        const val EMAIL_KEY = "email_key"
+        const val PASSWORD_KEY = "password_key"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val emailEditText = findViewById<EditText>(R.id.EditEmail)
+        val passwordEditText = findViewById<EditText>(R.id.editTextNumberPassword)
+        val loginButton = findViewById<Button>(R.id.button_login)
+
+        sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        val savedEmail = sharedPreferences.getString(EMAIL_KEY, null)
+        val savedPassword = sharedPreferences.getString(PASSWORD_KEY, null)
+        loginButton.setOnClickListener {
+
+            val name = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+if (name.isEmpty() || password.isEmpty())
+{
+    Toast.makeText(this, "Please Enter your Credentials", Toast.LENGTH_SHORT).show()
+}
+            else if (name == savedEmail && password == savedPassword) {
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            } else {
+
+                Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 }
--- EmptyViews_Screen/app/src/main/java/com/example/emptyviews_screen/RegisterScreen.kt
+++ EmptyViews_Screen/app/src/main/java/com/example/emptyviews_screen/RegisterScreen.kt
@@ -1,20 +1,90 @@
 package com.example.emptyviews_screen
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegisterScreen : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    companion object {
+        const val SHARED_PREFS = "shared_prefs"
+        const val EMAIL_KEY = "email_key"
+        const val PASSWORD_KEY = "password_key"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val emailEdt = findViewById<EditText>(R.id.editEmailReg)
+        val passwordEdt = findViewById<EditText>(R.id.editPasswordReg)
+        val registerBtn = findViewById<Button>(R.id.button_Reg)
+        val genderGroup = findViewById<RadioGroup>(R.id.radioGroupGender)
+        val countrySpinner = findViewById<Spinner>(R.id.spinnerCountry)
+        val termsCheck = findViewById<CheckBox>(R.id.checkTerms)
+        sharedPreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        val countries = arrayOf("Select Country", "Pakistan", "India", "USA", "UK")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countries)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        countrySpinner.adapter = adapter
+        registerBtn.setOnClickListener {
+
+            val email = emailEdt.text.toString()
+            val password = passwordEdt.text.toString()
+            val selectedGenderId = genderGroup.checkedRadioButtonId
+            val selectedCountry = countrySpinner.selectedItem.toString()
+            if (email.isEmpty()) {
+                emailEdt.error = "Email is required"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                passwordEdt.error = "Password is required"
+                return@setOnClickListener
+            }
+            if (selectedGenderId == -1) {
+                Toast.makeText(this, "Please select gender", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (selectedCountry == "Select Country") {
+                Toast.makeText(this, "Please select country", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (!termsCheck.isChecked) {
+                Toast.makeText(this, "Accept Terms & Conditions", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Save credentials
+            sharedPreferences.edit().apply {
+                putString(EMAIL_KEY, email)
+                putString(PASSWORD_KEY, password)
+                apply()
+            }
+            Toast.makeText(this, "Registration Successful! Please login.", Toast.LENGTH_SHORT).show()
+
+            // Move to LoginScreen and prevent back navigation
+            val intent = Intent(this, LoginScreen::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+
+
         }
     }
 }
